c9a0541c8b100093a0c39d53158584ef19cad244
Amy Ha <>
2021-08-20 10:52:21 +0100

    Create the FeedLoader protocol

---
+++ EssentialFieed/EssentialFieed/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentialFieed
+//
+//  Created by Amy Ha on 20/08/2021.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
