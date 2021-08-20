36f8d3ed2559cc43d4e9c461d613f427a4dcf2b4
Amy Ha <>
2021-08-20 08:57:54 +0100

    Create EssentialFeed framework

---
+++ EssentialFieed/EssentialFieed/EssentialFieed.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFieed.h
+//  EssentialFieed
+//
+//  Created by Amy Ha on 20/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFieed.
+FOUNDATION_EXPORT double EssentialFieedVersionNumber;
+
+//! Project version string for EssentialFieed.
+FOUNDATION_EXPORT const unsigned char EssentialFieedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFieed/PublicHeader.h>
+
+
---
+++ EssentialFieed/EssentialFieed/FeedItem.swift
@@ -0,0 +1,8 @@
+//
+//  FeedItem.swift
+//  EssentialFieed
+//
+//  Created by Amy Ha on 20/08/2021.
+//
+
+import Foundation
